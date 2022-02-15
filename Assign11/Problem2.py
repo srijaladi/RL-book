aa54b09bccb6f8e011d2e71c9d06df1956aa87b2
Epicsurya <96981697+srijaladi@users.noreply.github.com>
2022-02-15 09:50:09 -0800

    Completed Problem 1 and 2 in Assign11

---
+++ Assign11/Problem2.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from dataclasses import dataclass
+from typing import Optional, Mapping
+import numpy as np
+import itertools
+from rl.distribution import (Categorical, Distribution, FiniteDistribution,
+                             SampledDistribution, Constant)
+from rl.markov_process import MarkovProcess, NonTerminal, State, Terminal
+from rl.gen_utils.common_funcs import get_logistic_func, get_unit_sigmoid_func
+from rl.markov_process import FiniteMarkovProcess, MarkovRewardProcess, FiniteMarkovRewardProcess
+from rl.markov_decision_process import (FiniteMarkovDecisionProcess,
+                                        FiniteMarkovRewardProcess)
+from rl.policy import FinitePolicy, FiniteDeterministicPolicy
+from rl.chapter2.stock_price_simulations import\
+    plot_single_trace_all_processes
+from rl.chapter2.stock_price_simulations import\
+    plot_distribution_at_time_all_processes
+import matplotlib         
+from matplotlib import pyplot as plt
+from typing import (Callable, Dict, Iterable, Generic, Sequence, Tuple,
+                    Mapping, TypeVar, Set, List, Iterator)
+import itertools
+from itertools import combinations
+from itertools import product
+from rl.dynamic_programming import evaluate_mrp_result
+from rl.dynamic_programming import policy_iteration_result
+from rl.dynamic_programming import value_iteration_result
+from rl.dynamic_programming import value_iteration
+from typing import (Callable, Iterable, Iterator, Optional, TypeVar)
+from rl.iterate import converged, iterate, last
+from rl.markov_process import NonTerminal
+from rl.markov_decision_process import (FiniteMarkovDecisionProcess,
+                                        FiniteMarkovRewardProcess)
+from rl.dynamic_programming import value_iteration, almost_equal_vfs, greedy_policy_from_vf
+from pprint import pprint
+import rl.markov_process as mp
+from rl.approximate_dynamic_programming import (ValueFunctionApprox,
+                                                QValueFunctionApprox,
+                                                NTStateDistribution)
+S = TypeVar('S')
+A = TypeVar('A')
+
+
+
+
+
+
+def TabularTDPredict(
+        simulations: Iterable[Iterable[mp.TransitionStep[S]]],
+        gamma: float
+        ) -> Mapping[S, float]:
+    
+    V: Mapping[S, float] = []
+    stateUpdates: Mapping[S, int] = {}
+    
+    for currSim in simulations:
+        updateResult = updateTabularTD(currSim, gamma, V, stateUpdates)
+        stateUpdates = updateResult[0]
+        V = updateResult[1]
+    
+    return V
+    
+    
+def updateTabularTD(
+        currSim: Iterable[mp.TransitionStep[S]],
+        gamma: float,
+        currApprox: Mapping[S, float],
+        stateUpdates: Mapping[S, int]
+        ) -> Tuple[Mapping[S, int], Mapping[S, float]]:
+    
+    cumulativeReward = 0
+    
+    for transitionStep in reversed(currSim):
+        currState = transitionStep.state
+        
+        if isinstance(transitionStep.next_state, Terminal):
+            futureValue = 0
+        else:
+            futureValue = currApprox[transitionStep.next_state]
+            
+        currEstimate = transitionStep.reward + gamma * futureValue
+        
+        if stateUpdates.has_key(currState):
+            currApprox[currState] = currEstimate
+            stateUpdates[currState] = 1
+        else:
+            n = stateUpdates[currState]
+            change = (1/(n+1)) * (currEstimate - currApprox[currState])
+            currApprox[currState] += change
+            
+            stateUpdates[currState] += 1
+    
+    return (stateUpdates, currApprox)
+    
+    
+    
